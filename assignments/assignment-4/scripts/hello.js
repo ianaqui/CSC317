40c4d5bb67e8fa2e3a04b725e5acd73058b9f468
Adrian Aquino <aquinoa1103@gmail.com>
2025-04-10 01:34:35 -0700

    Assignment 4 Part 1 and 0

---
+++ assignments/assignment-4/scripts/hello.js
@@ -0,0 +1,11 @@
+// ES6 variable declaration
+const greeting = "JavaScript";
+
+// Arrow function (ES6 feature)
+const createMessage = (name) => {
+    // Template literal (ES6 feature)
+    return `Hello, ${name}!`;
+};
+
+// Output to console
+console.log(createMessage(greeting));
